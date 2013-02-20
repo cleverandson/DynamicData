685239a84696a85d16c0a6ded11900cd38aa9adb
Michael Gross <michael.gross@cleverandson.com>
2013-02-20 17:57:22 +0100

    R: gpl text included.

--- DynamicData/DDLoopReduce.h
+++ DynamicData/DDLoopReduce.h
@@ -1,10 +1,21 @@
-//
-//  DDLoopReduce.h
-//  DynamicData
-//
-//  Created by mich2 on 9/6/12.
-//  Copyright (c) 2012 -. All rights reserved.
-//
+/*
+ 
+    This file is part of DynamicData.
+
+    DynamicData is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ 
+*/
 
 #ifndef DynamicData_DDLoopReduce_h
 #define DynamicData_DDLoopReduce_h
